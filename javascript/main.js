f4fcd9f81ec389accea3249430387db48f3c470a
Zhakhov25 <lexas.zhakhov@gmail.com>
2021-07-22 19:00:47 +0300

    Yoga project

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay:true,
+        loop:true,
+        dots:false,
+        center: true,
+        margin: 30,
+        nav: true,
+        navText : ["",""],
+    });
+   });
